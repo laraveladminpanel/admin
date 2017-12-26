c06ad9c637fae14bbd32e2833a63b190676f9282
alexeykostenko <aleksey.kostenko.dev@gmail.com>
2017-12-27 00:38:38 +0200

    renamed voyager to admin

---
+++ publishable/database/seeds/VoyagerDummyDatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use LaravelAdminPanel\Traits\Seedable;
+
+class AdminDummyDatabaseSeeder extends Seeder
+{
+    use Seedable;
+
+    protected $seedersPath = __DIR__.'/';
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->seed('CategoriesTableSeeder');
+        $this->seed('UsersTableSeeder');
+        $this->seed('PostsTableSeeder');
+        $this->seed('PagesTableSeeder');
+        $this->seed('SettingsTableSeeder');
+        $this->seed('TranslationsTableSeeder');
+    }
+}
