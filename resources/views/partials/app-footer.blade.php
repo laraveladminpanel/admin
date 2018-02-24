244893f0d995f04d9d13cc4bcf29a40b20e88630
alexeykostenko <aleksey.kostenko.dev@gmail.com>
2018-02-24 22:30:42 +0200

    change footer

--- resources/views/partials/app-footer.blade.php
+++ resources/views/partials/app-footer.blade.php
@@ -1,10 +1,6 @@
 <footer class="app-footer">
     <div class="site-footer-right">
-        @if (rand(1,100) == 100)
-            <i class="admin-rum-1"></i> {{ __('admin.theme.footer_copyright2') }}
-        @else
-            {!! __('admin.theme.footer_copyright') !!} <a href="http://laraveladminpanel.com" target="_blank">Laravel Admin Panel</a>
-        @endif
+        {!! __('admin.theme.footer_copyright') !!} <a href="http://laraveladminpanel.com" target="_blank">Laravel Admin Panel</a>
         @php $version = Admin::getVersion(); @endphp
         @if (!empty($version))
             - {{ $version }}
