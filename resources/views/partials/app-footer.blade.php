e7483655a608f28e1905a1d5ec640e037892e50a
alexeykostenko <aleksey.kostenko.dev@gmail.com>
2018-04-05 17:24:57 +0300

    added custom copyright

--- resources/views/partials/app-footer.blade.php
+++ resources/views/partials/app-footer.blade.php
@@ -1,9 +1,13 @@
 <footer class="app-footer">
     <div class="site-footer-right">
-        {!! __('admin.theme.footer_copyright') !!} <a href="http://laraveladminpanel.com" target="_blank">Laravel Admin Panel</a>
-        @php $version = Admin::getVersion(); @endphp
-        @if (!empty($version))
-            - {{ $version }}
+        @if(setting('admin.copyright'))
+            {!! setting('admin.copyright') !!}
+        @else
+            {!! __('admin.theme.footer_copyright') !!} <a href="http://laraveladminpanel.com" target="_blank">Laravel Admin Panel</a>
+            @php $version = Admin::getVersion(); @endphp
+            @if (!empty($version))
+                - {{ $version }}
+            @endif
         @endif
     </div>
 </footer>
