cfa7462d4810e716cc65b269cb6dd675ab5d3e5b
alexeykostenko <aleksey.kostenko.dev@gmail.com>
2018-04-12 09:00:32 +0300

    fix the json format for a form designer

--- resources/views/formfields/custom/json_editor.blade.php
+++ resources/views/formfields/custom/json_editor.blade.php
@@ -1,6 +1,5 @@
 <div class="form-group">
-    <div id="{{ $row->field }}" data-theme="{{ @$options->theme }}" data-language="{{ @$options->language }}" class="ace_editor min_height_200"  data-name="{{ $row->display_name }}" name="{{ $row->field }}">@if(isset($dataTypeContent->{$row->field})){{ old($row->field, $dataTypeContent->{$row->field}) }}@elseif(isset($options->default)){{ old($row->field, $options->default) }}@else{{ old($row->field) }}@endif</div>
-    <textarea name="{{ $row->field }}" id="{{ $row->field }}_textarea" class="hidden">@if(isset($dataTypeContent->{$row->field})){{ old($row->field, $dataTypeContent->{$row->field}) }}@elseif(isset($options->default)){{ old($row->field, $options->default) }}@else{{ old($row->field) }}@endif</textarea>
+    <textarea id="json-input-{{ $row->field }}" class="resizable-editor" data-editor="json" data-name="{{ $row->display_name }}" name="{{ $row->field }}">@if(isset($dataTypeContent->{$row->field})){{ old($row->field, $dataTypeContent->{$row->field}) }}@elseif(isset($options->default)){{ old($row->field, $options->default) }}@else{{ old($row->field) }}@endif</textarea>
 </div>
 
 @section('javascript')
@@ -11,28 +10,6 @@
         window.invalidEditors = [];
 
         $(function () {
-
-            /**
-             * Reorder items
-             */
-            $('#crud-items').sortable({
-                handle: '.handler',
-                update: function (e, ui) {
-                    var _rows = $('#crud-items').find('.row_order'),
-                        _size = _rows.length;
-
-                    for (var i = 0; i < _size; i++) {
-                        $(_rows[i]).val(i+1);
-                    }
-                }
-            });
-
-            $('#crud-items').disableSelection();
-
-            $('[data-toggle="tooltip"]').tooltip();
-
-            $('.toggleswitch').bootstrapToggle();
-
             $('textarea[data-editor]').each(function () {
                 var textarea = $(this),
                 mode = textarea.data('editor'),
