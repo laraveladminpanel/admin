0d0660d37b9a3f406b21a134b9ddd1dc4a5fb1b7
alexeykostenko <aleksey.kostenko.dev@gmail.com>
2018-04-15 14:17:26 +0300

    change replace options to details

--- publishable/database/seeds/DataRowsTableSeeder.php
+++ publishable/database/seeds/DataRowsTableSeeder.php
@@ -966,10 +966,10 @@ class DataRowsTableSeeder extends Seeder
             ])->save();
         }
 
-        $dataRow = $this->dataRow($formDesignerDataType, 'details');
+        $dataRow = $this->dataRow($formDesignerDataType, 'options');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type'         => 'text',
+                'type'         => 'code_editor',
                 'display_name' => 'Options',
                 'required'     => 1,
                 'browse'       => 0,
@@ -977,7 +977,7 @@ class DataRowsTableSeeder extends Seeder
                 'edit'         => 1,
                 'add'          => 1,
                 'delete'       => 1,
-                'details'      => json_encode([
+                'options'      => json_encode([
                     'formfields_custom' => 'json_editor',
                 ]),
                 'order'        => 3,
