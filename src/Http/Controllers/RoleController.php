e3ae7da52b97894c242182c10a9c6493b323049a
Alexey Kostenko <aleksey.kostenko.dev@gmail.com>
2018-06-05 13:03:03 +0300

    change word BREAD to CRUD

--- src/Http/Controllers/RoleController.php
+++ src/Http/Controllers/RoleController.php
@@ -7,39 +7,7 @@ use LaravelAdminPanel\Facades\Admin;
 
 class RoleController extends CrudController
 {
-    // POST BR(E)AD
-    public function update(Request $request, $id)
-    {
-        $slug = $this->getSlug($request);
-
-        $dataType = Admin::model('DataType')->where('slug', '=', $slug)->first();
-
-        // Check permission
-        $this->authorize('edit', app($dataType->model_name));
-
-        //Validate fields with ajax
-        $val = $this->validateCrud($request->all(), $dataType->editRows);
-
-        if ($val->fails()) {
-            return response()->json(['errors' => $val->messages()]);
-        }
-
-        if (!$request->ajax()) {
-            $data = call_user_func([$dataType->model_name, 'findOrFail'], $id);
-            $this->insertUpdateData($request, $slug, $dataType->editRows, $data);
-
-            $data->permissions()->sync($request->input('permissions', []));
-
-            return redirect()
-            ->route("admin.{$dataType->slug}.index")
-            ->with([
-                'message'    => __('admin.generic.successfully_updated')." {$dataType->display_name_singular}",
-                'alert-type' => 'success',
-                ]);
-        }
-    }
-
-    // POST BRE(A)D
+    // POST (C)RUD
     public function store(Request $request)
     {
         $slug = $this->getSlug($request);
@@ -66,6 +34,38 @@ class RoleController extends CrudController
             ->route("admin.{$dataType->slug}.index")
             ->with([
                 'message'    => __('admin.generic.successfully_added_new')." {$dataType->display_name_singular}",
+                'alert-type' => 'success',
+                ]);
+        }
+    }
+
+    // POST CR(U)D
+    public function update(Request $request, $id)
+    {
+        $slug = $this->getSlug($request);
+
+        $dataType = Admin::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Check permission
+        $this->authorize('edit', app($dataType->model_name));
+
+        //Validate fields with ajax
+        $val = $this->validateCrud($request->all(), $dataType->editRows);
+
+        if ($val->fails()) {
+            return response()->json(['errors' => $val->messages()]);
+        }
+
+        if (!$request->ajax()) {
+            $data = call_user_func([$dataType->model_name, 'findOrFail'], $id);
+            $this->insertUpdateData($request, $slug, $dataType->editRows, $data);
+
+            $data->permissions()->sync($request->input('permissions', []));
+
+            return redirect()
+            ->route("admin.{$dataType->slug}.index")
+            ->with([
+                'message'    => __('admin.generic.successfully_updated')." {$dataType->display_name_singular}",
                 'alert-type' => 'success',
                 ]);
         }
